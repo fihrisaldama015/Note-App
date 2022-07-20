b567828931e6ccb5a2b34aaa6be860024c0713d8
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2022-07-21 03:03:36 +0700

    update when already added, fix input height, fix nav bg

--- pages/add.js
+++ pages/add.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "../components/navEdit";
 import { Color } from "../components/pallete";
 import { BsFillPatchCheckFill } from "react-icons/bs";
@@ -10,6 +10,13 @@ const Add = () => {
   const [index, setIndex] = useState(4);
   const [bg, setBg] = useState("white");
   const [isSaved, setIsSaved] = useState(false);
+  const [savedOnce, setsavedOnce] = useState(false);
+  const [id, setId] = useState();
+  const [height, setHeight] = useState(300);
+  const [heightTitle, setHeightTitle] = useState(60);
+  const ref = useRef(null);
+  const refTitle = useRef(null);
+
   const randomizeBg = () => {
     setIndex(index !== 4 ? index + 1 : 0);
     setBg(Color.list[index]);
@@ -18,6 +25,26 @@ const Add = () => {
   useEffect(() => {
     randomizeBg();
   }, []);
+
+  useEffect(() => {
+    isSaved && setsavedOnce(true);
+  }, [isSaved]);
+
+  useEffect(() => {
+    if (content) {
+      const element = ref.current;
+      // console.log(element.scrollHeight, "useEffect() 1");
+      setHeight(element.scrollHeight);
+    }
+  }, [content]);
+
+  useEffect(() => {
+    if (title) {
+      const element = refTitle.current;
+      // console.log(element.scrollHeight, "useEffect() 2");
+      setHeightTitle(element.scrollHeight);
+    }
+  }, [title]);
 
   const handleSubmit = async () => {
     let dateString = convertDate();
@@ -28,15 +55,22 @@ const Add = () => {
       bg,
     };
     try {
-      await axios.post("/api/note", data);
+      if (savedOnce) {
+        const updatedData = await axios.patch(`/api/note/${id}`, data);
+        // console.log(updatedData);
+      } else {
+        const savedData = await axios.post("/api/note", data);
+        // console.log(savedData.data._id);
+        setId(savedData.data._id);
+      }
       setIsSaved(true);
       setTimeout(() => {
         setIsSaved(false);
       }, 2000);
-      console.log("BERHASIL POST");
+      // console.log("BERHASIL POST");
     } catch (error) {
       console.log(error);
-      console.log("GAGAL POST");
+      // console.log("GAGAL POST");
     }
   };
   return (
@@ -44,9 +78,9 @@ const Add = () => {
       <Navbar
         randomize={() => randomizeBg()}
         submit={() => handleSubmit()}
-        transparent={bg !== "white"}
+        bg={`${Color.pallete[bg]}`}
       />
-      <form className="flex flex-col [&>*]:border-0 pt-24 xs:pt-0">
+      <form className="flex flex-col [&>*]:border-0 pt-24 xs:pt-0 min-h-screen">
         <div className="flex justify-center ">
           <div
             className={`${
@@ -54,24 +88,30 @@ const Add = () => {
             } absolute flex-row items-center gap-2 text-slate-800 bg-white w-min px-6 py-3 rounded-xl font-bold shadow-lg shadow-slate-500/10`}
             style={{ animation: "slideDown .2s" }}
           >
-            <BsFillPatchCheckFill className="fill-green-500" />
-            <p>Saved</p>
+            <div className="translate-x-[-0.5em] flex flex-row items-center gap-2">
+              <BsFillPatchCheckFill className="fill-green-500" />
+              <p>Saved</p>
+            </div>
           </div>
         </div>
-        <input
-          type="text"
+        <textarea
+          type="textarea"
           name="title"
           id="title"
+          style={{ height: `${heightTitle}px`, resize: "none" }}
+          ref={refTitle}
           className="text-5xl font-bold text-slate-800 tracking-wide focus:ring-0 overflow-auto bg-transparent"
           placeholder="Title..."
           onChange={(e) => setTitle(e.target.value)}
           value={title}
-        />
+        ></textarea>
         <textarea
           type="textarea"
           name="content"
           id="content"
-          className="text-2xl min-h-screen antialiased font-medium text-slate-800 focus:ring-0 leading-10 bg-transparent"
+          style={{ height: `${height}px`, resize: "none" }}
+          ref={ref}
+          className="text-2xl antialiased font-medium text-slate-800 focus:ring-0 leading-10 bg-transparent"
           placeholder="Write your notes here..."
           onChange={(e) => setContent(e.target.value)}
           value={content}
