b7164fa3767cf8bb42c0e290cef1f0daa81ca031
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2022-07-30 11:33:56 +0700

    fix UAid

--- pages/api/note/index.js
+++ pages/api/note/index.js
@@ -1,10 +1,24 @@
 import Note from "../../../utils/noteModel";
 
+const findUAid = (header) => {
+  let idIndex = 7;
+  for (let i = 0; i < header.length; i++) {
+    if (header[i] === "User-Agent") {
+      idIndex = i + 1;
+      break;
+    }
+  }
+  return idIndex;
+};
+
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       const allNote = await Note.find().sort({ updated_at: -1 });
       console.log("Data fetched...");
+
+      console.log("IP :", req.rawHeaders[1]);
+      console.log("UA :", req.rawHeaders[findUAid(req.rawHeaders)]);
       res.status(200).json(allNote);
     } catch (error) {
       console.log(error);
